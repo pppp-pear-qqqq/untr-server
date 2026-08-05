fb49ab6d8590e6beeacae120dbc9fc539e9c2d57
itt-n <itt_nakashima@worksid.co.jp>
2026-08-05 12:02:53 +0900

    フロント作り
    ログイン・新規登録フォームの見た目
    テーマ変更が維持されるように
    メニューバーの最低幅を設定

--- app/portal/resource/ts/common.ts
+++ app/portal/resource/ts/common.ts
@@ -1,2 +1,25 @@
 // theme
-document.querySelectorAll<HTMLElement>('[data-theme]:not(:root)').forEach(e => e.addEventListener('click', () => document.documentElement.dataset.theme = e.dataset.theme));
+document.querySelectorAll<HTMLElement>('[data-theme]:not(:root)').forEach(e => e.addEventListener('click', () => {
+	document.documentElement.dataset.theme = e.dataset.theme!;
+	localStorage.setItem('theme', e.dataset.theme!);
+}));
+
+// password visibility
+document.querySelectorAll<HTMLElement>('input[type="password"]+.toggle-visible').forEach(e => {
+	const input = e.previousElementSibling as HTMLInputElement;
+	e.addEventListener('click', () => {
+		input.type = input.type === 'password' ? 'text' : 'password';
+	});
+});
+
+// help dialog
+const dialog = document.getElementById('help') as HTMLDialogElement;
+document.querySelectorAll<HTMLElement>('.help').forEach(e => {
+	e.addEventListener('click', () => {
+		dialog.innerHTML = e.innerHTML;
+		dialog.showModal();
+	});
+});
+dialog.addEventListener('close', () => {
+	dialog.innerHTML = '';
+});
